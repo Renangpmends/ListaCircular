0509e091bda45fe7d57468efce919b1599bd7e32
Renangpmends <renan.pereira.mendes@gmail.com>
2024-12-04 10:12:49 -0300

    cirando os metodos add, get e remove da lista circular

---
+++ demo/src/main/java/listacircular/one/digitalinovation/ListaCircular.java
@@ -0,0 +1,57 @@
+package listacircular.one.digitalinovation;
+
+import listacircular.one.No;
+
+public class ListaCircular<T> {
+
+    private No<T> cabeca;
+    private No<T> cauda;
+    private int tamanhoLista;
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index){
+        if(this.isEmpty()){
+            throw new IndexOutOfBoundsException("A lista está vazia!");
+        }
+        if(index == 0){
+            return this.cauda;
+        }
+        No<T> noAuxiliar = this.cauda;
+        for(int i = 0; (i < index && (noAuxiliar != null)); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar; 
+    }
+
+    public void remove(int index){
+        if(index >= this.tamanhoLista){
+            throw new IndexOutOfBoundsException("O indice é maior que o tamanho da lista!");
+        }else{
+            No<T> noAuxiliar = this.cauda;
+            if(index == 0){
+                this.cauda = this.cauda.getNoProximo();
+                this.cabeca.setNoProximo(this.cauda);
+            }else if(index == 1){
+                this.cauda.setNoProximo(this.cauda.getNoProximo());
+            }else{
+                for(int i = 0; i < index-1; i++){
+                    noAuxiliar = noAuxiliar.getNoProximo();
+                }
+                noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+            }
+            this.tamanhoLista--;
+        }
+    }
+
+    public boolean isEmpty(){
+       return this.tamanhoLista == 0 ? true : false;
+    }
+
+    public int size(int index){
+        return this.tamanhoLista;
+    }
+
+}
